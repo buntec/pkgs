9b417f5d8d845bf9f2d47c173f1abad81a95a146
Christoph Bunte <christophbunte@gmail.com>
2023-11-04 17:54:14 +0100

    refactor

--- flake.nix
+++ flake.nix
@@ -16,66 +16,72 @@
         "x86_64-linux"
       ];
 
-    in {
-      packages = eachSystem (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ ];
-          };
-
-          buildCoursierApp =
-            pkgs.callPackage my-nix-utils.lib.mkBuildCoursierApp { };
-
-        in {
-          smithy-language-server = buildCoursierApp {
-            groupId = "software.amazon.smithy";
-            artifactId = "smithy-language-server";
-            version = "0.2.3";
-            depsHash = "sha256-yvBkxcUqAU4A2YlzFfUKYuKasNDZmkRZWRiEUTUphKc=";
-            javaOpts = [
-              "-XX:+UseG1GC"
-              "-XX:+UseStringDeduplication"
-              "-Xss4m"
-              "-Xms100m"
-            ];
-          };
-
-          smithy-cli = buildCoursierApp {
-            groupId = "software.amazon.smithy";
-            artifactId = "smithy-cli";
-            version = "1.39.1";
-            depsHash = "sha256-/8HW7ZhDBOXf6B/dDuqeMRjuE+mRa5hHkb524oqXLO0=";
-          };
-
-          smithy4s-cli = buildCoursierApp {
-            groupId = "com.disneystreaming.smithy4s";
-            artifactId = "smithy4s-codegen-cli_2.13";
-            version = "0.18.2";
-            pname = "smith4s-cli";
-            depsHash = "sha256-k5940JgN+RQTZLvPfAGwOmGn/9/KGUnq9WUKsGAoiPY=";
-          };
-
-          metals = buildCoursierApp {
-            groupId = "org.scalameta";
-            artifactId = "metals_2.13";
-            version = "1.0.1";
-            pname = "metals";
-            depsHash = "sha256-WAQbkBcYxGjWKdC2NZHHYPha9i+b7f+xWO1LVsLkJeI=";
-            javaOpts = [
-              "-XX:+UseG1GC"
-              "-XX:+UseStringDeduplication"
-              "-Xss4m"
-              "-Xms100m"
-            ];
-          };
-
-        });
-
-      overlays.default = final: prev: {
-        inherit (self.packages.${prev.system})
-          smithy-language-server smithy-cli smith4s-cli metals;
+      mkApp = { drv, name ? drv.pname or drv.name }: {
+        type = "app";
+        program = "${drv}/bin/${name}";
       };
+
+      buildCoursierApp = pkgs:
+        pkgs.callPackage my-nix-utils.lib.mkBuildCoursierApp { };
+
+      mkPackages = pkgs: {
+
+        smithy-language-server = buildCoursierApp pkgs {
+          groupId = "software.amazon.smithy";
+          artifactId = "smithy-language-server";
+          version = "0.2.3";
+          depsHash = "sha256-yvBkxcUqAU4A2YlzFfUKYuKasNDZmkRZWRiEUTUphKc=";
+          javaOpts = [
+            "-XX:+UseG1GC"
+            "-XX:+UseStringDeduplication"
+            "-Xss4m"
+            "-Xms100m"
+          ];
+        };
+
+        smithy-cli = buildCoursierApp pkgs {
+          groupId = "software.amazon.smithy";
+          artifactId = "smithy-cli";
+          version = "1.39.1";
+          depsHash = "sha256-/8HW7ZhDBOXf6B/dDuqeMRjuE+mRa5hHkb524oqXLO0=";
+        };
+
+        smithy4s-codegen-cli = buildCoursierApp pkgs {
+          groupId = "com.disneystreaming.smithy4s";
+          artifactId = "smithy4s-codegen-cli_2.13";
+          version = "0.18.2";
+          pname = "smithy4s-codegen-cli";
+          depsHash = "sha256-k5940JgN+RQTZLvPfAGwOmGn/9/KGUnq9WUKsGAoiPY=";
+        };
+
+        metals = buildCoursierApp pkgs {
+          groupId = "org.scalameta";
+          artifactId = "metals_2.13";
+          version = "1.0.1";
+          pname = "metals";
+          depsHash = "sha256-WAQbkBcYxGjWKdC2NZHHYPha9i+b7f+xWO1LVsLkJeI=";
+          javaOpts = [
+            "-XX:+UseG1GC"
+            "-XX:+UseStringDeduplication"
+            "-Xss4m"
+            "-Xms100m"
+          ];
+        };
+
+      };
+
+    in {
+
+      packages = eachSystem (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in mkPackages pkgs);
+
+      apps = eachSystem (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in builtins.mapAttrs (_: value: mkApp { drv = value; })
+        (mkPackages pkgs));
+
+      overlays.default = final: _: mkPackages final;
 
       checks = self.packages;
 
