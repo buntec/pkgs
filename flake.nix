65b3f7c6b96bee91cdf5fd5e2f8eb0512fb65914
Christoph Bunte <christophbunte@gmail.com>
2024-01-24 14:48:34 +0100

    update everything

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Some packages I find useful";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.my-nix-utils.url = "github:buntec/nix-utils";
+  inputs.nix-utils.url = "github:buntec/nix-utils";
 
-  outputs = { self, nixpkgs, my-nix-utils, ... }:
+  outputs = { self, nixpkgs, nix-utils, ... }:
 
     let
       inherit (nixpkgs.lib) genAttrs;
@@ -21,16 +21,15 @@
         program = "${drv}/bin/${name}";
       };
 
-      buildCoursierApp = pkgs:
-        pkgs.callPackage my-nix-utils.lib.mkBuildCoursierApp { };
+      overlays = [ nix-utils.overlays.default ];
 
       mkPackages = pkgs: {
 
-        smithy-language-server = buildCoursierApp pkgs {
+        smithy-language-server = pkgs.buildCoursierApp {
           groupId = "software.amazon.smithy";
           artifactId = "smithy-language-server";
-          version = "0.2.3";
-          depsHash = "sha256-r+hEvDG1KHcir14IsxDXoPC+ZDUkbPpVl0lNjAjquoI=";
+          version = "0.2.4";
+          depsHash = "sha256-CwYOqKrIB1lNpcLcIbs/WrwYtJ1c8CKr3I2pYi61C/g=";
           javaOpts = [
             "-XX:+UseG1GC"
             "-XX:+UseStringDeduplication"
@@ -39,27 +38,27 @@
           ];
         };
 
-        smithy-cli = buildCoursierApp pkgs {
+        smithy-cli = pkgs.buildCoursierApp {
           groupId = "software.amazon.smithy";
           artifactId = "smithy-cli";
-          version = "1.39.1";
-          depsHash = "sha256-YsDv4wUCA3bwUA88CvPOJqInE4AyC3HRDQlHvGbUuW4=";
+          version = "1.43.0";
+          depsHash = "sha256-fF5PeiWMcuWIUdMo9H5rdSNu3b+whlJ2VKqqJJOC7lM=";
         };
 
-        smithy4s-codegen-cli = buildCoursierApp pkgs {
+        smithy4s-codegen-cli = pkgs.buildCoursierApp {
           groupId = "com.disneystreaming.smithy4s";
           artifactId = "smithy4s-codegen-cli_2.13";
-          version = "0.18.2";
+          version = "0.18.6";
           pname = "smithy4s-codegen-cli";
-          depsHash = "sha256-QN/5ztcpZKoxBJ1BTpka7rkgBLQDSMdD5Zh4hCtUSJU=";
+          depsHash = "sha256-DlJDdiY6UotLUlyn1tFvP+1qNHUaacJNcFKKyQTkknM=";
         };
 
-        metals = buildCoursierApp pkgs {
+        metals = pkgs.buildCoursierApp {
           groupId = "org.scalameta";
           artifactId = "metals_2.13";
-          version = "1.0.1";
+          version = "1.2.0";
           pname = "metals";
-          depsHash = "sha256-2Y6u4hcV4o/4Fwi8mDbAQVlQ6yptRaAcB6BJ6oRPpkM=";
+          depsHash = "sha256-Izm9VVFOzHDecKlNlFmmE2rSCm7mus1QT3WfB/7ZryQ=";
           javaOpts = [
             "-XX:+UseG1GC"
             "-XX:+UseStringDeduplication"
@@ -73,11 +72,19 @@
     in {
 
       packages = eachSystem (system:
-        let pkgs = import nixpkgs { inherit system; };
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            inherit overlays;
+          };
         in mkPackages pkgs);
 
       apps = eachSystem (system:
-        let pkgs = import nixpkgs { inherit system; };
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            inherit overlays;
+          };
         in builtins.mapAttrs (_: value: mkApp { drv = value; })
         (mkPackages pkgs));
 
